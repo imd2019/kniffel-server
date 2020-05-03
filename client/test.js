598d05307e05c44845cd2be1d1e196ab5a3f7898
Lelo2000 <58939948+Lelo2000@users.noreply.github.com>
2020-05-03 16:58:57 +0200

    Room implemented
    + room join()
    + room create()
    + findRoom()

--- client/test.js
+++ client/test.js
@@ -1,5 +1,8 @@
 import Connection from "./connection.js";
 
-let connection = new Connection("test", "abcdefg");
+let connection = new Connection("test", "ILoveIMD2020");
 
 console.log(connection);
+let gameInfos = { name: "name", size: 4 };
+connection.socket.emit("createGame", gameInfos);
+connection.socket.emit("joinGame", gameInfos.name);
--- server/server.js
+++ server/server.js
@@ -9,23 +9,77 @@ let express = require("express");
 let app = express();
 const port = process.env.PORT || 3000;
 let server = app.listen(port);
-
+const password = "ILoveIMD2020";
 console.log("Server listening on port " + port + " ...");
 
 let socket = require("socket.io");
 let io = socket(server);
 
 global.SOCKET_LIST = {};
+let games = [];
 
 io.sockets.on("connection", newConnection);
 
 function newConnection(socket) {
   console.log("New player with id " + socket.id + " connected.");
-  console.log(socket.handshake.query);
-  SOCKET_LIST[socket.id] = socket;
+  //console.log(socket.handshake.query);
+  if (password === socket.handshake.query.pw) {
+    SOCKET_LIST[socket.id] = socket;
+    socket.name = socket.handshake.query.name;
+    console.log(
+      "Player with id " + socket.id + " set name to " + socket.name + "."
+    );
+  } else {
+    console.log(
+      "Player with id " +
+        socket.id +
+        " disconnected from Server. WRONG PASSWORD."
+    );
+    socket.disconnect();
+  }
+
+  socket.on("createGame", (gameInfos) => {
+    createGame(gameInfos, socket);
+  });
+
+  socket.on("joinGame", (gameName) => {
+    joinGame(gameName, socket);
+  });
 
   socket.on("disconnect", () => {
     console.log("Player with id " + socket.id + " disconnected.");
     delete SOCKET_LIST[socket.id];
   });
+}
+
+function createGame(gameInfos, socket) {
+  if (findGame(gameInfos.name) === false) {
+    games.push(gameInfos);
+    //console.log(games);
+    socket.join(gameInfos.name, function () {
+      console.log(
+        "Player " + socket.id + " created Room " + gameInfos.name + "."
+      );
+    });
+  } else {
+    console.log("Game exists already"); // EVTL noch zu einem Event für die Clientseite hinzufügen
+  }
+}
+
+function joinGame(gameName, socket) {
+  let game = findGame(gameName);
+  if (game != false) {
+    socket.join(gameName, function () {
+      console.log("Player " + socket.id + " joined Room " + gameName + ".");
+    });
+  }
+}
+
+function findGame(name) {
+  for (let index of games) {
+    if (index.name === name) {
+      return index;
+    }
+  }
+  return false;
 }
